a0ae30e4f9bf9346dd70d2cf8b1f1218ec05e21a
keijiokita <keiji.okita.ca@icloud.com>
2020-02-18 06:28:08 +0000

    rds added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-   def hello
-    render html: "Hello, 300!"
-   end
+   # def hello
+   #  render html: "Hello, 300!"
+   # end
   
   
 end
