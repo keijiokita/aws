ec2de8d83e41c3c9d208e4124194abeecd7a1e69
keijiokita <keiji.okita.ca@icloud.com>
2020-04-05 17:30:42 +0000

    user image column updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_171854) do
+ActiveRecord::Schema.define(version: 2020_04_05_172345) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(version: 2020_03_29_171854) do
     t.string "provider"
     t.string "uid"
     t.string "avatar"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
