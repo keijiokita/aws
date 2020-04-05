8017e26e76fabe649460fb47b8332abaff86bbd7
keijiokita <keiji.okita.ca@icloud.com>
2020-04-05 01:52:03 +0000

    female_models added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-1.upto(23) do |n|
+1.upto(10) do |n|
   Shot.create!(user_id: 1,
                user_shot: open("#{Rails.root}/db/fixtures/img#{n}.jpg"))
 end
+
+
+
+
+
+
+5.times do |n|
+  name = Faker::Name.female_first_name
+  password = "password"
+  User.create!(name: name,
+              email: "test#{n + 1}@test.com",
+              password: password,
+              password_confirmation: password,
+              image: File.open('./app/assets/images/model_#{n + 1}.png')
+  )
+end
+
+
+
+
+
+# Faker::Name.female_first_name
+
+
+# 100.times do |n|
+#   email = Faker::Internet.email
+#   password = "password"
+#   User.create!(email: email,
+#               password: password,
+#               password_confirmation: password,
+#               )
+# end
