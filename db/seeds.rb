3aa643a99b13c64b53732712aa116f5ff908ba92
keijiokita <keiji.okita.ca@icloud.com>
2020-04-05 17:00:46 +0000

    seeds.rb updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,67 +1,62 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create!(
-  [
-    {name: "Keiji",
-              email: "keiji.okita.ca@icloud.com",
-              password: "password",
-              password_confirmation: "password",
-              avatar: open("#{Rails.root}/db/fixtures/avatar/keijiokita.png")
-    },
-    {         name: "TestUser",
-              email: "testuser@gmail.com",
-              password: "testuser",
-              password_confirmation: "testuser",
-              avatar: open("#{Rails.root}/db/fixtures/avatar/testuser.png")
+# User.create!(
+#   [
+#     {name: "Keiji",
+#               email: "keiji.okita.ca@icloud.com",
+#               password: "password",
+#               password_confirmation: "password",
+#               avatar: open("#{Rails.root}/db/fixtures/avatar/keijiokita.png")
+#     },
+#     {         name: "TestUser",
+#               email: "testuser@gmail.com",
+#               password: "testuser",
+#               password_confirmation: "testuser",
+#               avatar: open("#{Rails.root}/db/fixtures/avatar/testuser.png")
      
-    }
-  ]
-)
+#     }
+#   ]
+# )
+
+
+# 10.times do |n|
+#   name = Faker::Name.female_first_name
+#   password = "password"
+#   User.create!(name: name,
+#               email: "#{n + 1}@test.com",
+#               password: password,
+#               password_confirmation: password,
+#               avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{n + 1}.png")
+#   )
+# end
+
+# 10.times do |n|
+#   name = Faker::Name.male_first_name
+#   password = "password"
+#   User.create!(name: name,
+#               email: "1#{n + 1}@test.com",
+#               password: password,
+#               password_confirmation: password,
+#               avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{100+(n + 1)}.png")
+#   )
+# end
+
 
 
 10.times do |n|
-  name = Faker::Name.female_first_name
-  password = "password"
-  User.create!(name: name,
-              email: "#{n + 1}@test.com",
-              password: password,
-              password_confirmation: password,
-              avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{n + 1}.png")
-  )
-end
-
-10.times do |n|
-  name = Faker::Name.male_first_name
-  password = "password"
-  User.create!(name: name,
-              email: "1#{n + 1}@test.com",
-              password: password,
-              password_confirmation: password,
-              avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{100+(n + 1)}.png")
-  )
-end
-
-
-100.times do |n|
-  id_number = Faker::Number.within(range: 1..10) 
-  shot_number = Faker::Number.within(range: 1..22) 
+  id_number = Faker::Number.within(range: 3..22) 
+  shot_number = Faker::Number.within(range: 1..50) 
   Shot.create!(user_id: id_number,
               user_shot: open("#{Rails.root}/db/fixtures/shot/img#{shot_number}.jpg")
               )
 end
 
-
-
-100.times do |n|
-  id_number = Faker::Number.within(range: 1..10) 
-  shot_number = Faker::Number.within(range: 1..22) 
+  
+10.times do |n|
+  id_number = Faker::Number.within(range: 3..22) 
+  shot_number = Faker::Number.within(range: 1..50) 
   comment = Faker::Quote.matz
   Comment.create!(user_id: id_number,
                   shot_id: shot_number,
