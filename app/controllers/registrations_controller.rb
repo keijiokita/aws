a9048d52c90ed6b095983912eefcdbae9f5671f3
keijiokita <keiji.okita.ca@icloud.com>
2020-04-06 20:33:24 +0000

    registration_controller updated

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -22,5 +22,12 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(_resource)
     edit_user_registration_path
   end
+  
 
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+  
 end
