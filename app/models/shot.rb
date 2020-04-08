8172009f925bb21b32aa7cf37f9ea729891b875a
keijiokita <keiji.okita.ca@icloud.com>
2020-04-08 15:14:49 +0000

    user_shot validation updated

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,7 +1,7 @@
 class Shot < ApplicationRecord
 	belongs_to :user
 	has_many :comments, dependent: :destroy
-	# validates :shot, presence: true
+	validates :user_shot, presence: true
 	
 	mount_uploader :user_shot, UserShotUploader
 	is_impressionable
