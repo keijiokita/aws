1c5de8558e44d074800cc3dbafff1cdc34a8445d
keijiokita <keiji.okita.ca@icloud.com>
2020-04-09 23:00:06 +0000

    pagenation order updated

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -8,7 +8,7 @@ class ShotsController < ApplicationController
   def index
     @shots = Shot.all.order('created_at DESC')
     # pagenation
-    @shots = Shot.page(params[:page]).per(24)
+    @shots = Shot.page(params[:page]).per(24).order('updated_at DESC')
   end
 
   # GET /shots/1
