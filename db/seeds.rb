db2bc0dcbd6354c2d644c1a2f771e62abcca2f43
keijiokita <keiji.okita.ca@icloud.com>
2020-04-10 00:41:45 +0000

    rubocop auto corrected

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 User.create!(
-     name: "Keiji",
-     email: "keiji.okita.ca@icloud.com",
-     password: "password",
-     password_confirmation: "password",
-     avatar: open("#{Rails.root}/db/fixtures/avatar/keijiokita.png")
+  name: "Keiji",
+  email: "keiji.okita.ca@icloud.com",
+  password: "password",
+  password_confirmation: "password",
+  avatar: open("#{Rails.root}/db/fixtures/avatar/keijiokita.png")
 )
 
 10.times do |n|
@@ -16,8 +16,7 @@ User.create!(
                email: "#{n + 1}@test.com",
                password: password,
                password_confirmation: password,
-               avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{n + 1}.png")
-              )
+               avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{n + 1}.png"))
 end
 
 10.times do |n|
@@ -27,8 +26,7 @@ end
                email: "1#{n + 1}@test.com",
                password: password,
                password_confirmation: password,
-               avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{100 + (n + 1)}.png")
-              )
+               avatar: open("#{Rails.root}/db/fixtures/avatar/model_#{100 + (n + 1)}.png"))
 end
 
 100.times do |_n|
@@ -38,13 +36,11 @@ end
                user_shot: open("#{Rails.root}/db/fixtures/shot/img#{shot_number}.jpg"))
 end
 
-
 200.times do |_n|
   id_number = Faker::Number.within(range: 2..22)
   shot_number = Faker::Number.within(range: 1..50)
   comment = Faker::Quote.matz
   Comment.create!(user_id: id_number,
                   shot_id: shot_number,
-                  response: comment
-                 )
+                  response: comment)
 end
