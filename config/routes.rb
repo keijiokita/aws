2828d6b5c4a564c8456de7e83de89a7da8ec36e5
keijiokita <keiji.okita.ca@icloud.com>
2020-06-21 04:15:43 +0000

    google maps added

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
  
  
   root 'shots#index'
+  
+  get 'maps/index', to: 'maps#index'
+  resources :maps, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
